af2a1441c95605d54d074c41884d472421969140
김진수 <100560249+greenClothesZelda@users.noreply.github.com>
2025-05-01 15:46:58 +0900

    socket통신 test까지 완료

--- app/routers/socket_cam.py
+++ app/routers/socket_cam.py
@@ -3,6 +3,8 @@
 from fastapi import APIRouter, WebSocket
 from starlette.websockets import WebSocketDisconnect
 from app.utils.logging import logger
+from asyncio import sleep
+import os
 
 router = APIRouter()
 
@@ -18,20 +20,24 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     logger.info(f"WebSocket connected: {websocket.client}")
     try:
-        while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
-            logger.info(data)
-
+        # 2초 대기 후 sample_audio.mp3 파일 전송
+        await sleep(2)
+        file_path = "videos/sample_audio.mp3"
+        if os.path.exists(file_path):
+            with open(file_path, "rb") as audio_file:
+                audio_data = audio_file.read()
+                await websocket.send_bytes(audio_data)
+                logger.info(f"Sent file: {file_path}")
+        else:
+            logger.error(f"File not found: {file_path}")
 
     except WebSocketDisconnect:
-
         logger.info("WebSocket disconnected")
 
     except Exception as e:
-
         logger.error(f"Unexpected error: {e}")
 
     finally:
         if not websocket.client_state.DISCONNECTED:
             await websocket.close()
+
