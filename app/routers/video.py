c863b98dd6d17c82c8ea65ff617fbe08c8510d7b
김진수 <100560249+greenClothesZelda@users.noreply.github.com>
2025-05-15 18:28:20 +0900

    세팅 저장

--- app/routers/video.py
+++ app/routers/video.py
@@ -1,13 +1,15 @@
 # WebSocket 및 HTTP 엔드포인트 정의
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Request
 from app.config import get_settings
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 import os
 from app.services.frame_extractor import get_frames
 from app.utils.logging import logger
 
 settings = get_settings()
 router = APIRouter()
+
+band_list = []
 
 @router.get("/video")
 async def get_video():
@@ -21,3 +23,19 @@ async def get_video():
         return Response(content=f"Video {video_path} not found.", status_code=404)
 
 
+@router.post("/setting/band")
+async def get_band(band_1: int, band_2: int, band_3: int, band_4: int):
+    """
+    Band 설정을 가져옵니다.
+    """
+    band = [band_1, band_2, band_3, band_4]
+    logger.info(f"Recieved Band {band}")
+    band_list.append(band)
+    return Response(content="Band setting received", status_code=200)
+
+@router.get("/setting/band")
+async def get_band_list():
+    """
+    Band 리스트를 JSON 형태로 반환합니다.
+    """
+    return JSONResponse(content={"band_list": band_list}, status_code=200)
